bacaaecc961abb6b5b52f3d9585f11914920dbe4
dagkanbilgin <hello@dagkanbilgin.com>
2024-06-17 22:07:14 +0300

    .

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import BioBox from './page-home/BioBox.vue'
+
 createApp(App).mount('#app')
+createApp(BioBox).mount('#biography')
