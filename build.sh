f72fc1ce384efef5ec498eaedb150af9bcb84f2c
dagkanbilgin <hello@dagkanbilgin.com>
2024-06-17 20:22:24 +0300

    .

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Öncelikle bağımlılıkları yükleyin
+npm install
+
+# Ardından build işlemini yapın
+npm run build
+
+# Başka bir build komutu gerekiyorsa ekleyin
+# yarn another-build-command
+
+# Diğer işlemler için gerekli komutlar
+# ./some-other-command
